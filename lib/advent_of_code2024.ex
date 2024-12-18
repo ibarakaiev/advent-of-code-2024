ed6c49642ae93ff6753e1828110ffd452a82f621
Igor Barakaiev <ibarakaiev@gmail.com>
2024-12-17 21:05:08 -0500

    add measure function

--- lib/advent_of_code2024.ex
+++ lib/advent_of_code2024.ex
@@ -2,4 +2,11 @@ defmodule AdventOfCode2024 do
   @moduledoc """
   Documentation for `AdventOfCode2024`.
   """
+
+  def measure(function) do
+    function
+    |> :timer.tc()
+    |> elem(0)
+    |> Kernel./(1_000_000)
+  end
 end
