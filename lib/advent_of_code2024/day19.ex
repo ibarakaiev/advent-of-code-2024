83b1095ca24f47bebd07c34f6fa0dd5b6741eec0
Igor Barakaiev <ibarakaiev@gmail.com>
2024-12-19 02:33:31 -0500

    day 19 with memoization

--- lib/advent_of_code2024/day19.ex
+++ lib/advent_of_code2024/day19.ex
@@ -1,20 +1,41 @@
 defmodule AdventOfCode2024.Day19 do
   @moduledoc false
-  def solve(input, part: 1) do
+
+  use Memoize
+
+  def solve(input, part: part) do
     {patterns, designs} = parse(input)
 
-    Enum.count(designs, &possible?(&1, patterns))
+    case part do
+      1 -> Enum.count(designs, &possible?(&1, patterns))
+      2 -> designs |> Enum.map(&total(&1, patterns)) |> Enum.sum()
+    end
   end
 
-  defp possible?("", _), do: true
+  defmemo(possible?("", _), do: true)
 
-  defp possible?(design, patterns) do
+  defmemo possible?(design, patterns) do
     Enum.any?(patterns, fn pattern ->
       if String.starts_with?(design, pattern) do
         ^pattern <> rest = design
         possible?(rest, patterns)
       end
     end)
+  end
+
+  defmemo(total("", _), do: 1)
+
+  defmemo total(design, patterns) do
+    patterns
+    |> Enum.map(fn pattern ->
+      if String.starts_with?(design, pattern) do
+        ^pattern <> rest = design
+        total(rest, patterns)
+      else
+        0
+      end
+    end)
+    |> Enum.sum()
   end
 
   defp parse(input) do
